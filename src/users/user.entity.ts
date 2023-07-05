ee05322091dc8b6e7d998e4b901974999b2dfba7
Ion <cristeaion36@gmail.com>
2023-07-05 20:46:44 +0300

    Course section 15

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -4,17 +4,24 @@ import {
   AfterUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { Report } from '../reports/report.entity';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   email: string;
+
   @Column()
   password: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
   @AfterInsert()
   logInserts() {
